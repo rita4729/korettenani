86c8c39be0bc7b85e30aae1cff80e7d73418d452
EC2 Default User <ec2-user@ip-172-31-39-19.ap-northeast-1.compute.internal>
2022-08-14 06:38:53 +0000

    Environment

--- api/config/application.rb
+++ api/config/application.rb
@@ -21,20 +21,14 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    config.time_zone = ENV["TZ"]
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :utc
+    
+    config.add_autoload_paths_to_load_path = false
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
     config.hosts << ""
